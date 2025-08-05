cc7d0b863b6904481dca7f03dd3ea73ad801df22
bakuuuuuuu <dudwls5674@naver.com>
2025-08-06 00:41:38 +0900

    [feat] 전적 요약 - 선호 포지션 UI 구현
    ItemsControl을 사용하여 선호 포지션 막대그래프 UI 구현
    
    ProgressBar 로드 시 차오르는 애니메이션 추가

---
+++ KDA/Models/PositionStats.cs
@@ -0,0 +1,12 @@
+﻿
+namespace KDA.Models
+{
+    public class PositionStats
+    {
+        // 아이콘 파일 경로
+        public string? IconPath { get; set; }
+
+        // 포지션 플레이 비율 (0~100)
+        public int PlayRate { get; set; }
+    }
+}
--- KDA/ViewModels/SearchResultViewModel.cs
+++ KDA/ViewModels/SearchResultViewModel.cs
@@ -5,7 +5,11 @@ namespace KDA.ViewModels
 {
     public class SearchResultViewModel
     {
+        // 사용자 프로필 정보
         public UserProfile UserProfileInfo { get; set; }
+
+        // 선호 포지션 정보
+        public List<PositionStats> PreferredPositions { get; set; }
 
         public SearchResultViewModel()
         {
@@ -18,9 +22,18 @@ namespace KDA.ViewModels
                 RankInfo = "KR | 래더 랭킹 418,634위",
                 Tier = "Platinum 1",
                 Lp = 36,
-                WinLossRecord = "63승 50패",
+                WinLossRecord = "163승 150패",
                 WinRate = 56,
                 LastUpdated = "최근 업데이트: 1분 전"
+            };
+
+            PreferredPositions = new List<PositionStats>
+            {
+                new PositionStats { IconPath = "/Resources/PositionIcons/top.png", PlayRate = 30 },
+                new PositionStats { IconPath = "/Resources/PositionIcons/jungle.png", PlayRate = 60 },
+                new PositionStats { IconPath = "/Resources/PositionIcons/mid.png", PlayRate = 25 },
+                new PositionStats { IconPath = "/Resources/PositionIcons/ad.png", PlayRate = 85 },
+                new PositionStats { IconPath = "/Resources/PositionIcons/support.png", PlayRate = 10 }
             };
         }
     }
